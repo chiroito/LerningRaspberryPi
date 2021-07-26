25cc710d909632908f55ccedfc0505e1873057bc
Chihiro Ito <chiroito107@gmail.com>
2021-07-26 17:53:55 +0900

    Chapter4 Initial

---
+++ chapter4/src/main/java/com/example/chapter4/Chapter4RestApplication.java
@@ -0,0 +1,11 @@
+package com.example.chapter4;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class Chapter4RestApplication extends Application {
+}
