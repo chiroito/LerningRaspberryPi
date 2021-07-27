1b1e45051ff648f48b4daa5f36bdafc63dd5ac45
Chihiro Ito <chiroito107@gmail.com>
2021-07-27 11:33:28 +0900

    no message

--- chapter4/src/main/java/com/example/chapter4/Chapter4CliApplication.java
+++ chapter4/src/main/java/com/example/chapter4/Chapter4CliApplication.java
@@ -1,43 +1,34 @@
 package com.example.chapter4;
 
-import com.pi4j.Pi4J;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.platform.Platforms;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
 import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 
+import java.util.concurrent.TimeUnit;
+
+@QuarkusMain
 public class Chapter4CliApplication implements  QuarkusApplication {
-
-    private static final int PIN_LED = 24; // PIN 18 = BCM 24
 
     @Override
     public int run(String... args) throws Exception {
-        var pi4j = Pi4J.newAutoContext();
-        Platforms platforms = pi4j.platforms();
 
-        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-//                .id("led")
-//                .name("LED Flasher")
-                .address(PIN_LED)
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW)
-//                .provider("pigpio-digital-output");
-;
+        final GpioController gpio = GpioFactory.getInstance();
+        final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22);
 
-        var led = pi4j.create(ledConfig);
-
-        int pressCount = 0;
-        while (pressCount < 5) {
-            if (led.equals(DigitalState.HIGH)) {
-                led.low();
-            } else {
+        for(int i = 0 ; i < 5 ; i++) {
+            if(led.isLow()) {
                 led.high();
+            }if(led.isHigh()){
+                led.low();
             }
-            Thread.sleep(500 / (pressCount + 1));
+
+            TimeUnit.SECONDS.sleep(1);
         }
-
-        pi4j.shutdown();
-
         return 0;
     }
 }
