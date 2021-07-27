46a1c16ab2a9a496124c2c6bcaeeba25e0b00d97
Chihiro Ito <chiroito107@gmail.com>
2021-07-27 16:12:33 +0900

    no message

--- chapter4/src/main/java/com/example/chapter4/Chapter4CliApplication.java
+++ chapter4/src/main/java/com/example/chapter4/Chapter4CliApplication.java
@@ -1,6 +1,5 @@
 package com.example.chapter4;
 
-
 import com.pi4j.io.gpio.*;
 
 import io.quarkus.runtime.QuarkusApplication;
@@ -9,18 +8,16 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 import java.util.concurrent.TimeUnit;
 
 @QuarkusMain
-public class Chapter4CliApplication implements  QuarkusApplication {
-
-    public static volatile boolean runningFlag = true;
+public class Chapter4CliApplication implements QuarkusApplication {
 
     @Override
     public int run(String... args) throws Exception {
 
         final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_25, "LED", PinState.HIGH);
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "LED", PinState.HIGH);
         pin.setShutdownOptions(true, PinState.LOW);
 
-        while(runningFlag) {
+        for(int i = 0 ; i < 10 ; i++) {
             pin.high();
             TimeUnit.MILLISECONDS.sleep(500);
             pin.low();
