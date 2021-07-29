49f3c974a234a89ee371e2d5a41f794481fddc99
Chihiro Ito <chiroito107@gmail.com>
2021-07-29 12:17:42 +0900

    7-01を修正

--- chapter7-01/src/main/java/com/example/chapter7/Chapter701CliApplication.java
+++ chapter7-01/src/main/java/com/example/chapter7/Chapter701CliApplication.java
@@ -18,14 +18,15 @@ public class Chapter701CliApplication implements QuarkusApplication {
     private static final byte register_adt7410 = 0x00;
 
     private double readAdt7410() throws IOException {
+
         int word_data = device.read(register_adt7410);
-        int data = ((word_data & 0xff00) >> 8) | (word_data & 0xff << 8);
+        int data = ((word_data & 0xff00) >> 8) | ((word_data & 0xff) << 8);
         data = data >> 3;
-        double temperature = 0;
+        double temperature = 0.0f;
         if( (data & 0x1000) == 0){
-            temperature = data * 0.0625;
+            temperature = ((double)data) * 0.0625f;
         } else {
-            temperature = ((~data & 0x1fff) + 1) * -0.0625;
+            temperature = ((double)((~data & 0x1fff) + 1)) * -0.0625f;
         }
         return temperature;
     }
@@ -34,12 +35,12 @@ public class Chapter701CliApplication implements QuarkusApplication {
     public int run(String... args) {
 
         try{
-            I2CBus i2cBus = I2CFactory.getInstance(I2CBus.BUS_0);
+            I2CBus i2cBus = I2CFactory.getInstance(I2CBus.BUS_1);
             this.device = i2cBus.getDevice(address_adt7410);
 
             while(true) {
                 double inputValue = readAdt7410();
-                System.out.println(inputValue);
+                System.out.println(String.format("%f", inputValue));
                 TimeUnit.MILLISECONDS.sleep(500);
             }
 
